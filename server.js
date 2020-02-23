8664b8ff3e861e34f740d206daf3214d17ef13c4
Satyam52 <satyam0350@gmail.com>
2020-02-23 23:33:51 +0530

    corrected

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 ///connect Database
 connectDB();
@@ -9,13 +10,20 @@ connectDB();
 //init bodyParser
 app.use(bodyParser.json({ extended: false }));
 
-app.get("/", (req, res, next) => {
-  res.send("RestAPI Running");
-});
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//serve static asset in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
