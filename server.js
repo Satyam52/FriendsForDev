2f44043dd508e69510359860f92693bbf34724c0
Satyam52 <satyam0350@gmail.com>
2020-02-23 23:49:15 +0530

    nn

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 
 //serve static asset in production
 if (process.env.NODE_ENV === "production") {
-  //set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
