e33df0a2994444a4695ec38876e379367b61221c
Satyam52 <satyam0350@gmail.com>
2020-02-24 01:28:07 +0530

    css to index

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import App from "./App";
+import "./App.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
