a2502fda9e6e139fabe053a61eff16416658c481
Satyam52 <satyam0350@gmail.com>
2020-02-24 01:53:39 +0530

    css to index

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -37,7 +37,7 @@ const Alert = ({ alerts }) => (
           key={alert.id}
           show
           fade
-          className={`alert-${alert.alertType}`}
+          className={`alert alert-${alert.alertType}`}
           iconClassName="text-primary"
           title="ALERT"
           message={alert.msg}
